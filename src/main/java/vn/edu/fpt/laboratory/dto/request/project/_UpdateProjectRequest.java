a2986e3833fef2a56004a15c9dd781e625320e6f
harleyhoang123 <harley.hoang.work@gmail.com>
2022-12-01 09:04:18 +0700

    Completed

--- src/main/java/vn/edu/fpt/laboratory/dto/request/project/_UpdateProjectRequest.java
+++ src/main/java/vn/edu/fpt/laboratory/dto/request/project/_UpdateProjectRequest.java
@@ -1,5 +1,12 @@
 package vn.edu.fpt.laboratory.dto.request.project;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
 /**
  * @author : Hoang Lam
  * @product : Charity Management System
@@ -7,5 +14,14 @@ package vn.edu.fpt.laboratory.dto.request.project;
  * @created : 30/11/2022 - 00:02
  * @contact : 0834481768 - hoang.harley.work@gmail.com
  **/
-public class _UpdateProjectRequest {
+@AllArgsConstructor
+@Data
+@Builder
+public class _UpdateProjectRequest implements Serializable {
+
+    private static final long serialVersionUID = -2000589070638322332L;
+    private String projectName;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate toDate;
 }
