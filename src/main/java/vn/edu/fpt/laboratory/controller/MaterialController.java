6f0f481fedbc81a701a916dbc2f1b41c4d1cbec5
vaduc <vaduc154@gmail.com>
2022-12-02 01:25:19 +0700

    Completed get material, project

--- src/main/java/vn/edu/fpt/laboratory/controller/MaterialController.java
+++ src/main/java/vn/edu/fpt/laboratory/controller/MaterialController.java
@@ -29,7 +29,24 @@ public interface MaterialController {
     ResponseEntity<GeneralResponse<Object>> addImage(@PathVariable(name = "material-id") String materialId, @RequestBody AddImageRequest request);
 
     @GetMapping
-    ResponseEntity<GeneralResponse<PageableResponse<GetMaterialResponse>>> getMaterial();
+    ResponseEntity<GeneralResponse<PageableResponse<GetMaterialResponse>>> getMaterial(
+            @RequestParam(name = "material-id", required = false) String materialId,
+            @RequestParam(name = "material-name", required = false) String materialName,
+            @RequestParam(name = "material-name-sort-by", required = false) String materialNameSortBy,
+            @RequestParam(name = "description", required = false) String description,
+            @RequestParam(name = "status", required = false) String status,
+            @RequestParam(name = "status-sort-by", required = false) String statusSortBy,
+            @RequestParam(name = "created-by", required = false) String createdBy,
+            @RequestParam(name = "created-date-from", required = false) String createdDateFrom,
+            @RequestParam(name = "created-date-to", required = false) String createdDateTo,
+            @RequestParam(name = "created-date-sort-by", required = false) String createdDateSortBy,
+            @RequestParam(name = "last-modified-by", required = false) String lastModifiedBy,
+            @RequestParam(name = "last-modified-date-from", required = false) String lastModifiedDateFrom,
+            @RequestParam(name = "last-modified-date-to", required = false) String lastModifiedDateTo,
+            @RequestParam(name = "last-modified-date-sort-by", required = false) String lastModifiedDateSortBy,
+            @RequestParam(name = "page", required = false) Integer page,
+            @RequestParam(name = "size", required = false) Integer size
+    );
 
     @GetMapping("/{material-id}")
     ResponseEntity<GeneralResponse<GetMaterialDetailResponse>> getMaterialId(@PathVariable(name = "material-id") String materialId);
