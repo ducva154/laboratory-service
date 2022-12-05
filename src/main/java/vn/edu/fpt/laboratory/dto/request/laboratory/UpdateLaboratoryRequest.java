ad692d4e92692d7221c39218d417c05e6c251910
harleyhoang123 <harley.hoang.work@gmail.com>
2022-12-05 10:34:55 +0700

    Fix response when update laboratory

--- src/main/java/vn/edu/fpt/laboratory/dto/request/laboratory/UpdateLaboratoryRequest.java
+++ src/main/java/vn/edu/fpt/laboratory/dto/request/laboratory/UpdateLaboratoryRequest.java
@@ -3,6 +3,7 @@ package vn.edu.fpt.laboratory.dto.request.laboratory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -14,10 +15,14 @@ import java.io.Serializable;
  * @contact : 0834481768 - hoang.harley.work@gmail.com
  **/
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class UpdateLaboratoryRequest implements Serializable {
     private static final long serialVersionUID = -4241640918694446022L;
     private String laboratoryName;
+    private String description;
+    private String major;
+    private String ownerBy;
 
 }
