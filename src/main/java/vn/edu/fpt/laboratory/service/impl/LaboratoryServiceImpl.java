f3823b6d199beaeec8ec99df3cf551ddbedb70bd
vaduc <vaduc154@gmail.com>
2022-12-11 00:47:16 +0700

    fix bug delete member in lab

--- src/main/java/vn/edu/fpt/laboratory/service/impl/LaboratoryServiceImpl.java
+++ src/main/java/vn/edu/fpt/laboratory/service/impl/LaboratoryServiceImpl.java
@@ -215,13 +215,20 @@ public class LaboratoryServiceImpl implements LaboratoryService {
             laboratory.setMembers(memberInfos);
             try {
                 laboratoryRepository.save(laboratory);
-                log.info("Remove permission from role success");
+                log.info("Remove member from lab success");
             } catch (Exception ex) {
-                throw new BusinessException(ResponseStatusEnum.INTERNAL_SERVER_ERROR, "Can't update labo in database after remove member");
+                throw new BusinessException(ResponseStatusEnum.INTERNAL_SERVER_ERROR, "Can't update lab in database after remove member");
             }
         } else {
-            throw new BusinessException(ResponseStatusEnum.INTERNAL_SERVER_ERROR, "Can't remove member from labo");
+            throw new BusinessException(ResponseStatusEnum.INTERNAL_SERVER_ERROR, "Can't remove member from lab");
         }
+        try {
+            memberInfoRepository.deleteById(memberId);
+            log.info("Remove member in database success");
+        } catch (Exception ex) {
+            throw new BusinessException(ResponseStatusEnum.INTERNAL_SERVER_ERROR, "Can't remove member in database");
+        }
+
     }
 
     private GetMemberResponse convertMemberToGetMemberResponse(MemberInfo memberInfo) {
